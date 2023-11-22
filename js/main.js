6cee1b7ed695e4d03f893a0a3114b6648e74711f
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-22 15:47:30 +0100

    array finished

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+//CREO ARRAY CON LE AUTO
+let cars = [
+    {
+        marca: "Fiat",
+        modello: "Punto",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "Fiat",
+        modello: "Panda",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "Opel",
+        modello: "Corsa",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "Opel",
+        modello: "Mokka",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "Fiat",
+        modello: "Bravo",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "Audi",
+        modello: "A4",
+        alimentazione: "ibrido"
+    },
+    {
+        marca: "Toyota",
+        modello: "Hylux",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "Nissan",
+        modello: "Micra",
+        alimentazione: "ibrida"
+    },
+    {
+        marca: "Ferrari",
+        modello: "Enzo",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "Lamborghini",
+        modello: "Murcielago",
+        alimentazione: "benzina"
+    },
+]
+console.log(cars)
